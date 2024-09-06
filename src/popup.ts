0faf027f8139fa798f6ea0f92ea3f217eb2facb3
auto-b64d <dmdjdj0213@gmail.com>
2024-09-06 15:15:44 +0900

    1.6

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,4 @@
 chrome.tabs.query({ active: true, currentWindow: true }).then(async ([tab]) => {
-	if (!/^\w+:\/\/arca\.live\//.test(tab.url!)) return
-	
 	await chrome.tabs.sendMessage(tab.id!, { type: 'content-download' })
 	
 	window.close()
