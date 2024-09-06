65d93e59c19d4d621af3bdaded3bb47b9043baed
auto-b64d <dmdjdj0213@gmail.com>
2024-09-06 15:26:17 +0900

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -3,10 +3,12 @@ chrome.runtime.onMessage.addListener(msg => {
 	
 	if (!document.querySelector('.article-wrapper')) return // 글 페이지가 아님
 	
+	const url = new URL(document.querySelector<HTMLAnchorElement>('.article-link a')!.href)
+	
 	chrome.runtime.sendMessage({
 		type: 'sw-download',
-		url: location.href,
-		articleId: location.pathname.split('/').at(-1),
+		url: url.href,
+		articleId: url.pathname.split('/').at(-1),
 		articleTitle: [...document.querySelector('.article-head .title')!.childNodes].at(-1)!.textContent!.trim(),
 		articleBodyHtml:
 			document.querySelector('.article-body')!
